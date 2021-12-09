f140bf6b165f52e25792c3a95eb40f7270b990cc
Guilherme <guilherme.silva@mpt.mp.br>
2021-12-09 14:38:13 -0300

    version 0.2

--- ball.js
+++ ball.js
@@ -1,5 +1,5 @@
 const INITIAL_VEL=0.035
-const SPEED_INCREMENT=0.005
+const SPEED_INCREMENT=0.002
 
 export default class Ball {
     constructor(element){
@@ -41,20 +41,37 @@ export default class Ball {
         }
         addEventListener('mousemove', this.mouseListener)
     }
-
+    
     start(){
         // this.direction = 1
-        let heading = randomNumberBetween(Math.PI/6, Math.PI*5/6)
-        this.direction = {x: Math.cos(heading), y: -Math.sin(heading)}
+        // let heading = randomNumberBetween(Math.PI/6, Math.PI*5/6)
+        
+        this.heading = Math.PI/4
+        let multiplier = Math.random()>0.5?1:-1
+        console.log('🚀 ~ file: ball.js ~ line 115 ~ start ~ multiplier', multiplier)
+        this.direction = {x: Math.cos(this.heading)*multiplier, y: -Math.sin(this.heading)}
         this.velocity = INITIAL_VEL
         this.inGame = true
         // this.update(delta,paddleRects)
         removeEventListener('mousemove', this.mouseListener)
     }
 
-    
-    update(delta, paddleRects, blocks){
-    
+    updateDirection(salt){
+        console.log('🚀 ~ file: ball.js ~ line 107 ~ updateDirection ~ salt', salt)
+        // console.log('--',5*Math.PI/6)
+        // console.log('--',Math.PI/6)
+        // console.log('h+s',this.heading+salt)
+        if ((this.heading+salt)<5*Math.PI/12 && (this.heading+salt)>Math.PI/12){
+            this.heading = Math.abs(this.heading+salt)
+            let multiplier = (this.direction.x/Math.abs(this.direction.x))
+            console.log('🚀 ~ file: ball.js ~ line 115 ~ updateDirection ~ multiplier', multiplier)
+            this.direction = {x: Math.cos(this.heading)*multiplier, y: -Math.sin(this.heading)}
+        }
+    }
+
+    update(delta, paddle, blocks){
+        // console.log('heading ==> ',this.heading)
+        // console.log('directionx ==> ',this.direction.x)
         this.x+=this.direction.x*this.velocity*delta
         this.y+=this.direction.y*this.velocity*delta
         if(this.rect().top<0){
@@ -79,8 +96,15 @@ export default class Ball {
                 setTimeout(tmpBlock.collided(),250)
                 this.velocity+=SPEED_INCREMENT
         }
-        if (this.isCollision(this.rect(), paddleRects)){
+        if (this.isCollision(this.rect(), paddle.rect())){
             this.direction.y*=-1
+            // this.direction.x*=-1
+            // if(this.x<paddle.position){
+                this.updateDirection((paddle.position-this.x)/20)
+            // }
+            // else{
+                // this.updateDirection((this.x-paddle.position)/20)
+            // }
         }
 
         if(this.rect().bottom>(window.innerHeight+50)){
@@ -91,8 +115,8 @@ export default class Ball {
     }
 
     isCollision(ball, other){
-        console.log("🚀 ~ file: ball.js ~ line 94 ~ isCollision ~ other", other)
-        console.log("🚀 ~ file: ball.js ~ line 94 ~ isCollision ~ ball", ball)
+        // console.log("🚀 ~ file: ball.js ~ line 94 ~ isCollision ~ other", other)
+        // console.log("🚀 ~ file: ball.js ~ line 94 ~ isCollision ~ ball", ball)
         return ball.left <= other.right &&
             ball.right >= other.left &&
             ball.top <= other.bottom &&
